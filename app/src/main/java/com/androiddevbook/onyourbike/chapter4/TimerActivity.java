6b312c3f43b11b7fd5e80adecc28430edbf7a9bb
Chris Lamb <cclamb@ece.unm.edu>
2015-05-04 06:18:01 -0600

    Moving through chapter 5. I wish this used events to pass information rather than call semantics.

--- app/src/main/java/com/androiddevbook/onyourbike/chapter4/TimerActivity.java
+++ app/src/main/java/com/androiddevbook/onyourbike/chapter4/TimerActivity.java
@@ -90,7 +90,8 @@ public final class TimerActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_timer, menu);
+        //getMenuInflater().inflate(R.menu.menu_timer, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
@@ -99,14 +100,13 @@ public final class TimerActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.menu_settings:
+                onClickSettings(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void onClickStart(View view) {
