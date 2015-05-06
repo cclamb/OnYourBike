2eb6910623f6677f7390a7b91c0df4b066ba6911
Chris Lamb <cclamb@ece.unm.edu>
2015-05-06 06:35:48 -0600

    Added home button to settings activity; needed to use getSupportActionBar(.) rather than getActionBar(.), which returned null

--- app/src/main/java/com/androiddevbook/onyourbike/chapter4/SettingsActivity.java
+++ app/src/main/java/com/androiddevbook/onyourbike/chapter4/SettingsActivity.java
@@ -1,7 +1,12 @@
 package com.androiddevbook.onyourbike.chapter4;
 
+//import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,6 +14,8 @@ import android.widget.CheckBox;
 
 
 public final class SettingsActivity extends ActionBarActivity {
+
+    private final String ME = getClass().getName();
 
     private CheckBox vibrate;
 
@@ -19,6 +26,14 @@ public final class SettingsActivity extends ActionBarActivity {
         vibrate = (CheckBox) findViewById(R.id.vibrate_checkbox);
         Settings settings = ((YetiTimezApplication) getApplication()).getSettings();
         vibrate.setChecked(settings.isVibrateOn());
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
@@ -41,14 +56,13 @@ public final class SettingsActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                gotoHome();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void vibrateChanged(View view) {
@@ -63,5 +77,14 @@ public final class SettingsActivity extends ActionBarActivity {
 
     public void goBack(View view) {
         finish();
+    }
+
+    private void gotoHome() {
+        Log.d(ME, "gotoHome");
+
+        Intent timer = new Intent(getApplicationContext(), TimerActivity.class);
+        timer.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(timer);
     }
 }
